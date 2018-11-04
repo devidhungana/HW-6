5606e99d02bdb7876ab1ad74d205c75e8700ae18
devidhungana <43016940+devidhungana@users.noreply.github.com>
2018-11-04 02:25:07 -0500

    Add files via upload

---
+++ ContactModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace ContactManager.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your contact page.";
+        }
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        Contact.OwnerID = UserManager.GetUserId(User);
+
+        // requires using ContactManager.Authorization;
+        var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                                                    User, Contact,
+                                                    ContactOperations.Create);
+        if (!isAuthorized.Succeeded)
+        {
+            return new ChallengeResult();
+        }
+
+        Context.Contact.Add(Contact);
+        await Context.SaveChangesAsync();
+
+        return RedirectToPage("./Index");
+    }
+}
---
+++ CreateModel.cs
@@ -0,0 +1,15 @@
+﻿using ContactManager.Data;
+using ContactManager.Pages.Contacts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+public class CreateModel : DI_BasePageModel
+{
+    public CreateModel(
+        ApplicationDbContext context,
+        IAuthorizationService authorizationService,
+        UserManager<IdentityUser> userManager)
+        : base(context, authorizationService, userManager)
+    {
+    }
+}
